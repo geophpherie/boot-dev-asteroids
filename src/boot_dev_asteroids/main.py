7b5abc66b0f7b5bb87c5e72eea389e5429f177d8
geophpherie <jeff.beyer@gmail.com>
2024-08-25 21:31:58 -0700

    window not showing

--- src/boot_dev_asteroids/main.py
+++ src/boot_dev_asteroids/main.py
@@ -1,5 +1,5 @@
 import pygame
-from boot_dev_asteroids import constants
+from boot_dev_asteroids import constants, player
 
 
 def main():
@@ -8,12 +8,22 @@ def main():
         size=(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
     )
 
+    # clock = pygame.time.Clock()
+    # dt = 0
+
+    # p = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=pygame.Color(0, 0, 0))
+
+        screen.fill("black")
         pygame.display.flip()
+
+        # p.draw(screen)
+
+        # dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
